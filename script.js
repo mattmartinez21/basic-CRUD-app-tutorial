d825f5134c72eca60d30b2418c88b32b7f536333
Matt Martinez <mattmartinez21@gmail.com>
2020-03-04 08:55:35 -0500

    basic functionality of js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 function onFormSubmit() {
-    var formData = readFormData();
-
+  var formData = readFormData();
+  insertNewRecord(formData);
 }
 
 function readFormData() {
-    var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
-    return formData;
+  var formData = {};
+  formData["fullName"] = document.getElementById("fullName").value;
+  formData["empCode"] = document.getElementById("empCode").value;
+  formData["salary"] = document.getElementById("salary").value;
+  formData["city"] = document.getElementById("city").value;
+  return formData;
 }
 
 function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4
+  var table = document
+    .getElementById("employeeList")
+    .getElementsByTagName("tbody")[0];
+  var newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.fullName;
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.empCode;
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.salary;
+  cell4 = newRow.insertCell(3);
+  cell4.innerHTML = data.city;
+  cell4 = newRow.insertCell(4);
+  cell4.innerHTML = `<a>Edit</a><a>Delete</a>;`;
 }
